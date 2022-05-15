0dd374640c7914e1bbf23b34867c721ebc1b9427
V-Ivanich <chipai_dom@mail.ru>
2022-05-15 20:31:23 +0300

    minimum fix

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 
+import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Route, Routes} from 'react-router-dom';
 import NaviBar from './Components/Navibar';
 
-function App() {
+import {Home} from './Home';
+import {Users} from './Users';
+import {About} from './About';
+
+const Case1 = () => {
   return (
-    <>
-    <NaviBar />
-    </>
-  );
+    <Routes>
+    <Route path='/' element={<Home />} />
+    <Route path='/users' element={<Users />} />
+    <Route path='/about' element={<About />} />
+    </Routes>
+  )
 }
 
+function App() {
+   return (
+     
+    <BrowserRouter>
+    <NaviBar />
+      <Case1 />
+    </BrowserRouter>
+  );
+}
 export default App;
--- src/Components/Navibar.js
+++ src/Components/Navibar.js
@@ -1,22 +1,24 @@
 
 import React from "react";
 import { Nav, Navbar, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 function NaviBar() {
-
+  let mar = {marginLeft: '15px'}
+  let mar2 = {marginRight: '15px'}
   return (
   <>
-    <Navbar expand='lg' bg='dark' variant='dark'>
-      <Navbar.Brand >WebDev Blog</Navbar.Brand>
+    <Navbar expand='md' bg='dark' variant='dark'>
+      <Navbar.Brand style={mar}>WebDev Blog</Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className='me-auto'>
-          <Nav.Link>Home</Nav.Link>
-          <Nav.Link>Users</Nav.Link>
-          <Nav.Link>About</Nav.Link>
+          <Nav.Link><Link to='/'>Home</Link></Nav.Link>
+          <Nav.Link><Link to='/users'>Users</Link></Nav.Link>
+          <Nav.Link><Link to='/about'>About</Link></Nav.Link>
         </Nav>
-        <Nav>
-          <Button variant='primary' className='mr-2'>Log In</Button>
+        <Nav style={mar2}>
+          <Button variant='primary' className='me-2'>Log In</Button>
           <Button variant='primary'>Sing out</Button>
         </Nav>
       </Navbar.Collapse>
